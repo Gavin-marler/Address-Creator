634205a78a1d87f5bd4139eac1230373e590fc21
Gavin Marler <65262617+Gavin-marler@users.noreply.github.com>
2020-09-30 13:09:04 -0600

    update

--- address.py
+++ address.py
@@ -7,11 +7,36 @@ city = input("City: ")
 state = input("State: ")
 zip = input("Zip Code: ")
 print()
-def creator(name, add1, add2, city, state, zip):
-    print(name) 
-    print(add1)
+
+def checkadd(add1, add2):
+    if len(add1) > 50:
+        print("Address Line 1 Too Long")
+    else:
+        print(add1)
     if add2 != "":
         print(add2)
-    print("{}, {} {}".format(city, state, zip))
+
+def checkcity(city):
+    if len(city) > 50:
+        return print("City Too Long")
+    else:
+        return city
+
+def checkstate(state):
+    if len(state) > 12:
+        return print("State too Long")
+    else:
+        return state
+
+def checkzip(zip):
+    if len(zip) > 5:
+        return str("Invalid Zip Code")
+    else:
+        return zip
+
+def creator(name, add1, add2, city, state, zip):
+    print(name)
+    checkadd(add1, add2) 
+    print("{}, {} {}".format(checkcity(city), checkstate(state), checkzip(zip)))
 
 creator(name, add1, add2, city, state, zip)
